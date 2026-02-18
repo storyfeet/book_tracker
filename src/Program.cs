d7ffae928bbd1838dcf24dbd9344d506b5f8e2a0
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-02-18 10:53:34 +0000

    mv Program into src

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+using  Microsoft.AspNetCore.Http;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/hello", (HttpRequest request)=>{
+    var query = request.Query;
+    var name = query["name"];
+    return $"Hello to {name}";
+} );
+
+app.Run();
