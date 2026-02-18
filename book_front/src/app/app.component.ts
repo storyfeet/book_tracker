695477cfeb8bbd6fa074933af8c071506de31661
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-02-18 13:26:29 +0000

    Create Book Component

--- book_front/src/app/app.component.ts
+++ book_front/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookComponent } from './book/book.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ book_front/src/app/book/book.component.ts
@@ -0,0 +1,12 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  imports: [],
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.scss',
+})
+export class BookComponent {
+  @Input() title!:string;
+  @Input() link!:string;
+}
