b27de0115913a6197c41b1a78a7c1df692bb90c9
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-02-23 15:22:31 +0000

    Add flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Ghostscript enabled flake";
+
+  inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-25.05-darwin";
+	#nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+	let 
+		legacy = nixpkgs.legacyPackages.x86_64-linux;	
+	in {
+
+	packages.x86_64-linux = {
+		hello = legacy.hello;
+		ghostscript = legacy.ghostscript;
+		starship = legacy.starship;
+		default = legacy.nushell;
+		
+	};   
+
+	environment.systemPackages = with legacy; [
+		ghostscript
+		nushell
+		starship
+	];
+
+	devShells.x86_64-linux = {
+		default = legacy.mkShell {
+			buildInputs = [
+				legacy.dotnetCorePackages.sdk_9_0_1xx-bin	
+				legacy.dotnet-ef
+				legacy.ghostscript
+				legacy.starship
+				legacy.evince
+				legacy.nushell
+				legacy.nodejs_24
+				legacy.nodePackages."@angular/cli"
+			];
+			shellHook = ''
+				echo "Running hooks"
+				exec nu
+			'';
+		};
+
+	};
+    
+  };   
+	
+}
