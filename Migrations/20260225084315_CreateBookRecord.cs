ac2dcdbc31d2892b87f0249b310d3ba7656e6b8c
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-02-25 08:45:45 +0000

    Create Books Migration

---
+++ Migrations/20260225084315_CreateBookRecord.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace book_tracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateBookRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Link = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- src/models/Book.cs
+++ src/models/Book.cs
@@ -3,13 +3,19 @@ namespace Models;
 using Microsoft.Data.Sqlite;
 
 public class Book {
-    public string Id {get;set;}
+    public long Id {get;set;}
     public string Title {get; set;}
     public string Link {get;set;}
 
+    public Book(long Id, string Title , string Link ){
+        this.Id = Id;
+        this.Title = Title;
+        this.Link = Link;
+    }
+
     // If select *
     public Book(SqliteDataReader reader ){
-        this.Id = reader.GetString(0);
+        this.Id = int.Parse(reader.GetString(0));
         this.Title = reader.GetString(1);
         this.Link = reader.GetString(2);
     }
