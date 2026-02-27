a9c195940e36ae00ed5ee8e8a09110c2a37d0460
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-02-27 12:32:42 +0000

    Add About Sidebar

--- book_front/src/app/app.component.ts
+++ book_front/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BookComponent } from './book/book.component';
 import { BookListerService } from './services/book-lister.service';
 import { Book } from '../data/book_data';
-import { BookAddFormComponent } from "./book-add-form/book-add-form.component";
 import { BookTabBoxComponent } from './book-tab-box/book-tab-box.component';
+import { SideBarComponent } from "./side-bar/side-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BookComponent, BookAddFormComponent, BookTabBoxComponent],
+  imports: [RouterOutlet, SideBarComponent,BookTabBoxComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ book_front/src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  imports: [],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.scss'
+})
+export class SideBarComponent {
+  show:boolean = false;
+  
+}
