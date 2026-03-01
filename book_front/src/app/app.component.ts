87ccb56ca35174598c0a83aff271aee8f8bb4ff0
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-03-01 10:29:56 +0000

    Add route path for book edit

--- book_front/src/app/app.component.ts
+++ book_front/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BookTabBoxComponent } from './components/book-tab-box/book-tab-box.component';
-import { SideBarComponent } from "./components/side-bar/side-bar.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideBarComponent,BookTabBoxComponent, SideBarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- book_front/src/app/app.routes.ts
+++ book_front/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomePageComponent,
+    },
+    {
+        path:'local/books/edit',
+        component:HomePageComponent,
+    }
+
+];
