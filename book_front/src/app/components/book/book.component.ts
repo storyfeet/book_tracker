ede4f46e42154736e527cf3e93ed33b8d93141cc
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-03-01 17:43:07 +0000

    Allow Edit click on Book Listing

--- book_front/src/app/components/book/book.component.ts
+++ book_front/src/app/components/book/book.component.ts
@@ -1,9 +1,10 @@
 import { Component,Input } from '@angular/core';
 import { Book } from '../../data/book_data';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
 })
